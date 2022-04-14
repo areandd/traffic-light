af8f9d7f341d93b4b38f06f4bfd33e07e3ae73d9
Isaiah Desta <areandd@protonmail.com>
2022-04-14 00:48:59 +0000

    beginings of a button that wraps the main functionality.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,31 @@ import React, { useState } from "react";
 const Home = () => {
 	const [selectedColor, setSelectedColor] = useState("red");
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={
-					"light red" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-				className={
-					"light green" + (selectedColor === "green" ? " glow" : "")
-				}></div>
+		<div>
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" + (selectedColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" + (selectedColor === "green" ? " glow" : "")
+					}></div>
+			</div>
+				<button className="btn btn-primary m-3">
+				Click to Start Lights
+				</button>
+</div>
 		</div>
+		
+		
 	);
 };
 
